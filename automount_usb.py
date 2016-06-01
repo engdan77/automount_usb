cf8724593fc98184e929614a1ab71efb23cbb0f5
chrome <daniel@engvalls.eu>
2016-06-01 10:31:14 +0200

    Final version

--- automount_usb.py
+++ automount_usb.py
@@ -1,6 +1,7 @@
 from subprocess import check_output, STDOUT
 import re
 import os
+import argparse
 
 
 def get_devs_by_mount(input, prefixes=['/dev/mm.', '/dev/mapper']):
@@ -26,8 +27,26 @@ def get_connected_devs(_dir='/dev', prefixes=['/dev/mm.', '/dev/mapper']):
     return result
 
 
+def mount_device(dev, mountroot='/dev/mnt'):
+    """Mount device"""
+    dev = dev.split('/')[-1]
+    dev_dir = '{}/{}'.format(mountroot, dev)
+    if not os.path.isdir(dev_dir):
+        os.mkdir(dev_dir)
+    check_output('mount {} {}'.format(dev, dev_dir))
+
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Application for mounting unmounted devices')
+    parser.add_argument('--device_prefixes', help='default: /dev/sd[a-z][1-9]', metavar='device', default='/dev/sd[a-z][1-9]')
+    parser.add_argument('--mount_root', metavar='dir', help='default: /mnt', default='/mnt')
+    parser.add_argument('--device_dir', metavar='dir', help='default: /dev/', default='/dev')
+    args = parser.parse_args()
     mounts = check_output('mount', stderr=STDOUT)
-    mounted_devs = get_devs_by_mount(mounts)
-    connected_devs = get_connected_devs()
-    print connected_devs
+    mounted_devs = get_devs_by_mount(mounts, prefixes=args.device_prefixes)
+    connected_devs = get_connected_devs(_dir=args.device_dir, prefixes=args.device_prefixes)
+
+    for connected_dev in connected_devs:
+        if not connected_dev in mounted_devs:
+            print 'Mounting {}'.format(connected_dev)
+            mount_device(connected_dev)
